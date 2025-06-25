169b800740b7acb33cf88d740e34d91c3c2cba45
Heru Handika <herubiolog@gmail.com>
2025-06-25 17:33:27 -0400

    Add skeletons.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,12 @@
+use std::time::Instant;
+
+use clap::Parser;
+
+use crate::cli::img::Cli;
+
 pub mod img;
+
+pub fn parse_cli() {
+    let time = Instant::now();
+    let args = Cli::parse();
+}
--- src/img/resize.rs
+++ src/img/resize.rs
@@ -1,6 +1,5 @@
 pub fn test_resize() {
     // This function is a placeholder for testing the resize functionality.
     // You can implement your resizing logic here.
-
     println!("Testing image resize functionality.");
 }
